18abb57017204b44fd8f55c01768d96c62d3447a
7NelNel7 <daniel.tejtej.1@educa.jcyl.es>
2024-10-09 11:16:25 +0200

    Agregar cosas ej dni

---
+++ algo.js
@@ -0,0 +1,18 @@
+
+function comprobarDni(dni){
+    let dniInt = "";
+    let letras = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
+    for (let i = 0; i < dni.length - 1; i++) {
+        dniInt += dni[i];
+    }
+    let respuesta = "";
+    if (letras[dniInt % 23] == dni[8]) {
+        respuesta = "Tu DNI está bien"; 
+    } else {
+        respuesta = "Está mal";
+    } 
+    console.log(respuesta);
+    return respuesta;
+}
+
+
